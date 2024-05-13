f5a915adfe19a2968bd2a8a8d3f4cdd73e997c24
sultanTrash <115166309+sultanTrash@users.noreply.github.com>
2024-05-12 20:30:05 -0400

    Issue #2 StrategieForet implementée
    Lire les commentaires pour BannirCase
    TRÈS IMPORTANT:
    La distance entre les arbres est modifiable dans l'éditeur!

---
+++ Assets/Scripts/StrategieForet/StrategieForet.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StrategieForet
+{
+    public const int PremiereCaseX = -60;   // Premiere case en x, (y=0),z en haut à gauche à laquelle il peut se trouver un arbre
+    public const int PremiereCaseZ = -60;
+    public abstract void GenererForet(float delimitionArbres, GameObject ArbrePrefab, GameObject[] arbres);
+    public virtual void BannirCase(bool[,] tab)
+    {
+        /*
+         * Comme on travaille avec des distances entre arbres variantes, on ne peut pas hard-code les delimitations des zones bannis.
+         * Nous allons donc procéder avec des ratios, et toujours arrondir vers le haut.
+         * Prenons les délimitations dans le cas ou la distance entre chaque arbre est de 4, et qu'il y a donc 30 par 30 arbres sur la carte.
+         * 
+         * Les générations commencent à partir de en haut à gauche et génère row par row.
+         * On commence donc avec l'origine -60,-60
+         * 
+         * Délimitations du village:
+         *      x: [-60,-40] donc après [0,20] cases a partir de l'origine (-60,-60)
+         *      z: [-29, 0], donc après [31,60] cases a partir de l'origine (-60,-60)
+         *      La zone bannie est un rectangle x:0,  z:31
+         *      jusqu'à                         x:20, z:60
+         *      Donc, dans le cas qu'on aurait un tableau 120 par 120
+         *      0,0 serait équivalent aux coordonnées -60, -60
+         *      On ferait : if ( (i>= 0 && i<=20) && (j>=31 && j<=60) ) {tab[i,j] = false}
+         *      Ca nous fait donc un ratio de 0/120, 20/120 pour les i et 31/120, 60/120 pour les j
+         *      
+         *      Si l'on convertit ça à notre example de 30 par 30 arbres:
+         *      En faisant : int(Math.RoundUp(0*30/120))
+         *      x: [0,5] ; z : [8,15]
+         *
+         * Notons donc les délimitations de toute les zones bannis:
+         * Village : x[-60,-40] ; z[-29,0].   Ajustées : x[0,20]    ; z[31,60]
+         * Maison  : x[45, 60]  ; z[-60,-45]. Ajustées : x[105,120] ; z[0 ,15]
+         * Chemin1 : x[55, 60]  ; z[-45, -15].Ajustées : x[115,120] ; z[15,45]
+         * Chemin2 : x[-40, 60] ; z[-18,-12]. Ajustées : x[20, 120] ; z[42,48]
+         */
+        for (int i = 0; i < tab.GetLength(0); i++)
+        {
+            for (int j=0; j < tab.GetLength(1); j++)
+            {
+                if (VerifierCaseBannie(i, j, tab.GetLength(0)))
+                {
+                    tab[i, j] = false;
+                }
+            }
+        }
+    }
+    public virtual bool VerifierCaseBannie(int i, int j, int tailleTab)
+    {
+        bool banned = false;
+
+        // Village : x[0,20] ; z[31,60]
+        if ((i >= 0 && i <= Mathf.CeilToInt(20.0f / 120 * tailleTab)) &&
+            (j >= Mathf.CeilToInt(31.0f / 120 * tailleTab) && j <= Mathf.CeilToInt(60.0f / 120 * tailleTab)))
+        {
+            banned = true;
+        }
+
+        // Maison : x[105,120] ; z[0,15]
+        if ((i >= Mathf.CeilToInt(105.0f / 120 * tailleTab) && i <= Mathf.CeilToInt(120.0f / 120 * tailleTab)) &&
+            (j >= 0 && j <= Mathf.CeilToInt(15.0f / 120 * tailleTab)))
+        {
+            banned = true;
+        }
+
+        // Chemin1 : x[115,120] ; z[15,45]
+        if ((i >= Mathf.CeilToInt(115.0f / 120 * tailleTab) && i <= Mathf.CeilToInt(120.0f / 120 * tailleTab)) &&
+            (j >= Mathf.CeilToInt(15.0f / 120 * tailleTab) && j <= Mathf.CeilToInt(45.0f / 120 * tailleTab)))
+        {
+            banned = true;
+        }
+
+        // Chemin2 : x[20,120] ; z[42,48]
+        if ((i >= Mathf.CeilToInt(20.0f / 120 * tailleTab) && i <= Mathf.CeilToInt(120.0f / 120 * tailleTab)) &&
+            (j >= Mathf.CeilToInt(42.0f / 120 * tailleTab) && j <= Mathf.CeilToInt(48.0f / 120 * tailleTab)))
+        {
+            banned = true;
+        }
+
+        return banned;
+    }
+    public abstract void GenererArbresForet(GameObject[] arbres, bool[,] TableauArbreBool, float delimitionArbres, GameObject ArbrePrefab);
+}
---
+++ Assets/Scripts/StrategieForet/StrategieGrille.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrategieGrille : StrategieForet
+{
+    public override void GenererForet(float delimitionArbres, GameObject ArbrePrefab, GameObject[] arbres)
+    {
+        float tailleTableauFloat = 120 / delimitionArbres;      // Pourquoi 120? Afin de simplifier les divisions, les delimitation des arbres sont à 60 et -60 (x et z).
+                                                                // Si par exemple chaque arbre occupe un espace de 4x4, il y aurait donc un total de 30 par 30 arbres
+        int tailleTableau = (int)Math.Round(tailleTableauFloat);// Il y a donc 120 unités possible.
+        bool[,] TableauForet = new bool[tailleTableau,tailleTableau];
+        InitAllTrue(TableauForet);
+        BannirCase(TableauForet);                               // Met à false les cases ou il ne devrait pas avoir d'arbres.
+        GenererArbresForet(arbres, TableauForet,delimitionArbres,ArbrePrefab);
+    }
+    private void InitAllTrue(bool[,] tab)
+    {
+        for (int i = 0; i < tab.GetLength(0); i++)
+        {
+            for (int j = 0; j < tab.GetLength(1); j++)
+            {
+                tab[i,j] = true;
+            }
+        }
+    }
+    public override void GenererArbresForet(GameObject[] arbres, bool[,]TableauArbreBool, float delimitionArbres, GameObject ArbrePrefab)
+    {
+        foreach (GameObject arbre in arbres)
+        {
+            GameObject.Destroy(arbre);
+        }
+        for (int i = 0; i < TableauArbreBool.GetLength(0); i++)
+        {
+            for (int j = 0; j < TableauArbreBool.GetLength(1); j++)
+            {
+                if (TableauArbreBool[i, j]) // Vérifie si un arbre doit être placée ici.
+                {
+                    Vector3 position = new Vector3(PremiereCaseX + (delimitionArbres * i), 0, (PremiereCaseZ + (delimitionArbres * j)));
+                    // Transforme la position de l'arbre à partir de la position de la case dans le tableau original.
+                    GameObject.Instantiate(ArbrePrefab, position, Quaternion.identity);
+                }
+            }
+        };
+    }
+
+}

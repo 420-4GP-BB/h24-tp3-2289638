ba23454b8c4b8b605f5c09b468bf7c580e144c1a
sultanTrash <115166309+sultanTrash@users.noreply.github.com>
2024-05-13 01:20:27 -0400

    Issue #2 Game of Life balance

--- Assets/Scripts/StrategieForet/StrategieGOL.cs
+++ Assets/Scripts/StrategieForet/StrategieGOL.cs
@@ -16,6 +16,7 @@ public class StrategieGOL : StrategieForet
         for (int i=0;i<10;i++)
         {
             TableauForet = GameOfLifeRound(TableauForet);
+            BannirCase(TableauForet);
         }
         GenererArbresForet(arbres, TableauForet, delimitionArbres, ArbrePrefab);
     }
